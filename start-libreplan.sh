db4336867b673dbc685e484ea13bf0132708a39a
Tim Sutton <tim@kartoza.com>
2015-07-15 13:40:38 +0200

    Revert "Fix for db initialisation"
    This reverts commit 47caf130f38711feb2af6542496110ee84b182e5.

--- start-libreplan.sh
+++ start-libreplan.sh
@@ -2,8 +2,8 @@
 
 export PGHOST="$DB_PORT_5432_TCP_ADDR"
 export PGPORT="$DB_PORT_5432_TCP_PORT"
-export PGPASSWORD="docker"
-export PGUSER=docker
+export PGPASSWORD="$DB_ENV_POSTGRES_PASSWORD"
+export PGUSER=postgres
 
 polling_interval=3
 echo "wait for postgres to start first..."
@@ -16,11 +16,12 @@ do
 done
 
 if [[ ! `psql -lqt | grep '^ libreplan\b'` ]]; then
-  echo "CREATE DATABASE gis;\
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;" | psql;
+  echo "CREATE DATABASE libreplan;\
+    CREATE USER libreplan WITH PASSWORD 'libreplan';\
+    GRANT ALL PRIVILEGES ON DATABASE libreplan TO libreplan;" | psql;
 
   wget -q -O install.sql http://downloads.sourceforge.net/project/libreplan/LibrePlan/install_1.4.0.sql
-  PGPASSWORD=docker psql -U docker -f install.sql gis;
+  PGPASSWORD=libreplan psql -U libreplan -f install.sql;
 fi
 
 catalina.sh run
